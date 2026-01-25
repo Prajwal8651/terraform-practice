957cf8281e2abe610b1daa2a160c775004d456d0
Prajwal8651 <prajwalgowda112001@gmail.com>
2026-01-25 23:34:53 +0530

    day13

---
+++ lessons/day13/main.tf
@@ -0,0 +1,39 @@
+data "aws_vpc" "vpc_name" {
+    filter {
+      name = "tag:Name"
+      values = ["default"]
+
+    }
+}
+
+data "aws_subnet" "shared" {
+    filter {
+      name = "tag:Name"
+      values = ["shared"]
+    }
+    vpc_id = data.aws_vpc.vpc_name.id
+}
+
+data "aws_ami" "linux2" {
+    owners = ["amazon"]
+    most_recent = true
+
+    filter {
+      name = "name"
+      values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+    filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+    }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.linux2.id
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet.shared.id
+
+  tags = {
+    Name = "Day13-WebServer"
+  }
+}
