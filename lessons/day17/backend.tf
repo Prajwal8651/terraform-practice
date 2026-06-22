9f0d5d11246b8e0f29d1f082577c8123dacac957
Prajwal P <prajwalgowda112001@gmail.com>
2026-06-22 22:51:22 +0530

    added day 17

---
+++ lessons/day17/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "terraform-state-1766811759"
+    key          = "day14ls/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
